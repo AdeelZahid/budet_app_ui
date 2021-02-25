5a856b366d1bccbb911a1fd8d6900839ccbc103e
AdeelZahid <m.adeel2144@gmail.com>
2021-02-25 15:54:09 +0500

    Bar Chart has been added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,8 +11,55 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("hello Flutter"),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 100.0,
+            forceElevated: true,
+            // floating: true,
+            pinned: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+              centerTitle: true,
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 100.0,
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 6.0,
+                        offset: Offset(0, 4),
+                        color: Colors.black12,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
       ),
     );
   }
