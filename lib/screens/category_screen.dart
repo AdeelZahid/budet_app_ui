713b0a64b08d1b459070f7fed989333313d66c31
AdeelZahid <m.adeel2144@gmail.com>
2021-02-26 12:37:12 +0500

    Budget App is done Now

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,10 @@
-import 'package:budget_app/data/data.dart';
+import 'dart:developer';
+import 'dart:math';
+
+import 'package:budget_app/herlpers/color_healper.dart';
 import 'package:budget_app/models/category_model.dart';
+import 'package:budget_app/models/expense_model.dart';
+import 'package:budget_app/widgets/radial_panter.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -10,8 +15,68 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpense() {
+    List<Widget> expenseList = [];
+    widget.category.expenses.forEach(
+      (Expense expense) {
+        expenseList.add(
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            height: 80.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    expense.name,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '-\$${expense.cost.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    return Column(
+      children: expenseList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -43,7 +108,25 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   ),
                 ],
               ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            _buildExpense(),
           ],
         ),
       ),
